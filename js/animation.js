69888a3507eda6697fa7f2aa4b6146709b03e7e6
Ayush Gupta <721ayush@gmail.com>
2020-06-10 12:49:05 +0530

    update-all

--- js/animation.js
+++ js/animation.js
@@ -3,13 +3,16 @@
  *   All rights reserved.
  */
     var images = [
+         "pic-1.jpg",
+      "pic-2.jpg",
+      "pic-3.jpg",
     "close-up-photography-of-eyeglasses-at-golden-hour-690887.jpg",
     "both-hands-stained-with-paints-1161542.jpg",
     "abstract-art-blaze-bonfire-266526.jpg",
       "aaron-burden-6jYoil2GhVk-unsplash.jpg",
       "matt-ragland-02z1I7gv4ao-unsplash.jpg",
       "photo-1509569785882-4c160654309e.jpg",
-      "honey-yanibel-minaya-cruz-laORtJZaieU-unsplash.jpg",
+     
       "3618264.jpg",
       "neven-krcmarek-V4EOZj7g1gw-unsplash.jpg",
     ];
--- js/script2.js
+++ js/script2.js
@@ -10,10 +10,33 @@ $(document).ready(function(){
     /** click event on toggle menu */
     $toggleCollapse.click(function () {
         $nav.toggleClass('collapse');
-    })
+    });
 
     $('.sub-btn').click(function(){
         window.alert('Message Sent!!!!!')
-    })
+    });
+
+    
 
 });
+
+ mybutton = document.getElementById("mybtn");
+
+ window.onscroll = function () {
+   scrollFunction();
+ };
+ function scrollFunction() {
+   if (
+     document.body.scrollTop > 20 ||
+     document.documentElement.scrollTop > 20
+   ) {
+     mybutton.style.display = "block";
+   } else {
+     mybutton.style.display = "none";
+   }
+ }
+
+ function topfunction() {
+   document.body.scrollTop = 0;
+   document.documentElement.scrollTop = 0;
+ }
